d1ac775822ec661b3ab020823a0fdd4509858ef4
SteinerDiamine <shubhamsharma68663@gmail.com>
2024-03-17 00:34:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Highlights from './components/Highlights';
+
+const  App = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <main className='bg-black'>
+      <Navbar/>
+      <Hero/>
+      <Highlights/>
+    </main>
+)
+}
+
+export default App
